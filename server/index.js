e56881327930062fcd5327d460a81ff13ce4b159
ekirillov <kiregr777@mail.ru>
2021-05-05 23:59:12 +0300

    Initial server setup

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import { ApolloServer, gql } from "apollo-server";
+import { weatherSample } from "./weather.js";
+
+const typeDefs = gql`
+  type CurrentWeather {
+    city: String
+    temperature: Float
+  }
+
+  type Query {
+    currentWeather: CurrentWeather
+  }
+`;
+
+const resolvers = {
+  Query: {
+    currentWeather: () => ({
+      temperature: weatherSample.main.temp,
+      city: weatherSample.name,
+    }),
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
